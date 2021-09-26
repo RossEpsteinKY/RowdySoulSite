602833c19d1f221d1bea02734db7e5361ba42bf8
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-09-26 17:24:24 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    MDBBootstrapModule.forRoot(),
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
