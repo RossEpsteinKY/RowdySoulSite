dcae0d0339a2acab0ab144d2f1603278a81b05e9
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-09-26 17:42:23 -0400

    added home page and navbar elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import { NavbarComponent } from './navbar/navbar.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
     BrowserModule,
-    RouterModule
+    RouterModule.forRoot(routes),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
